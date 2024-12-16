63b731c67c74a332cd889b76d8a4b610afc90ef1
三苟 Sango <letusgoone@outlook.com>
2024-12-16 11:45:15 +0800

    修改logging.hpp，移除 table.hpp

--- include/cango/logging.hpp
+++ include/cango/logging.hpp
@@ -1,8 +1,7 @@
 #pragma once
 
 #include <cango/logging/combine.hpp>
+#include <cango/logging/make_logger.hpp>
 #include <cango/logging/rolling.hpp>
 #include <cango/logging/std_impl.hpp>
-#include <cango/logging/table.hpp>
 #include <cango/logging/wrap.hpp>
-#include <cango/logging/make_logger.hpp>
